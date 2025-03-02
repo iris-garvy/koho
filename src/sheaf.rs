b0729686e53ef269f8cbb5dab28c865183c253df
Estelle Sterrett <estellesterrett@gmail.com>
2025-03-02 15:41:22 -0500

    full k-laplacian?

--- src/sheaf.rs
+++ src/sheaf.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
 use crate::{
-    algebra::{Field, Matrix},
+    algebra::{add_vectors, Field, Matrix},
     cw::{KCell, Skeleton},
     error::Error,
 };
@@ -42,7 +42,7 @@ impl<F: Field> Sections<F> {
 }
 pub struct CellularSheaf<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> {
     pub cw: Skeleton<T, O>,
-    pub data: Vec<Sections<F>>,
+    pub section_spaces: Vec<Sections<F>>,
     pub restrictions: HashMap<(usize, usize), Matrix<F>>,
     pub global_sections: Vec<(Sections<F>)>,
 }
@@ -51,7 +51,7 @@ impl<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> CellularSheaf<F, T, 
     pub fn init() -> Self {
         Self {
             cw: Skeleton::init(),
-            data: Vec::new(),
+            section_spaces: Vec::new(),
             restrictions: HashMap::new(),
             global_sections: Vec::new(),
         }
@@ -62,7 +62,7 @@ impl<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> CellularSheaf<F, T, 
         cell: Box<dyn KCell<T, O>>,
         data: Option<Sections<F>>,
     ) -> Result<(), Error> {
-        self.data.push(if data.is_some() {
+        self.section_spaces.push(if data.is_some() {
             data.unwrap()
         } else {
             Sections::new()
@@ -72,21 +72,21 @@ impl<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> CellularSheaf<F, T, 
     }
 
     pub fn update(&mut self, cell_idx: usize, data_idx: usize, val: Vec<F>) -> Result<(), Error> {
-        if cell_idx >= self.data.len() {
+        if cell_idx >= self.section_spaces.len() {
             return Err(Error::InvalidCellIdx);
         }
-        if data_idx >= self.data[cell_idx].data.len() {
+        if data_idx >= self.section_spaces[cell_idx].data.len() {
             return Err(Error::InvalidDataIdx);
         }
-        self.data[cell_idx].data[data_idx] = val;
+        self.section_spaces[cell_idx].data[data_idx] = val;
         Ok(())
     }
 
     pub fn new_data(&mut self, cell_idx: usize, val: Vec<F>) -> Result<(), Error> {
-        if cell_idx >= self.data.len() {
+        if cell_idx >= self.section_spaces.len() {
             return Err(Error::InvalidCellIdx);
         }
-        self.data[cell_idx].data.push(val);
+        self.section_spaces[cell_idx].data.push(val);
         Ok(())
     }
 
@@ -99,19 +99,23 @@ impl<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> CellularSheaf<F, T, 
         if start_cell >= self.cw.cells.len() || final_cell >= self.cw.cells.len() {
             return Err(Error::InvalidCellIdx);
         }
+        if self.cw.cells[start_cell].cell.dimension() <= self.cw.cells[final_cell].cell.dimension()
+        {
+            return Err(Error::NoRestrictionDefined);
+        }
         self.restrictions.insert((start_cell, final_cell), map);
         Ok(())
     }
 
     pub fn k_coboundary(
-        &mut self,
+        &self,
         cell_idx: usize,
         data_idx: usize,
     ) -> Result<Vec<(Vec<F>, usize)>, Error> {
         if cell_idx >= self.cw.cells.len() {
             return Err(Error::InvalidCellIdx);
         }
-        if data_idx >= self.data[cell_idx].data.len() {
+        if data_idx >= self.section_spaces[cell_idx].data.len() {
             return Err(Error::InvalidDataIdx);
         }
         let mut results = Vec::new();
@@ -123,7 +127,7 @@ impl<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> CellularSheaf<F, T, 
             let restrict = restriction.unwrap();
             results.push((
                 restrict
-                    .transform(&self.data[cell_idx].data[data_idx])
+                    .transform(&self.section_spaces[cell_idx].data[data_idx])
                     .unwrap(),
                 i,
             ));
@@ -132,7 +136,7 @@ impl<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> CellularSheaf<F, T, 
     }
 
     pub fn k_coboundary_adjoint(
-        &mut self,
+        &self,
         cell_idx: usize,
         cochain: Vec<(Vec<F>, usize)>,
     ) -> Result<Vec<F>, Error> {
@@ -140,7 +144,7 @@ impl<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> CellularSheaf<F, T, 
             return Err(Error::InvalidCellIdx);
         }
         let idxs = cochain.iter().map(|(_, x)| *x).collect::<Vec<_>>();
-        let domain_bases = &self.data[cell_idx].bases;
+        let domain_bases = &self.section_spaces[cell_idx].bases;
         let mut results = Vec::new();
         for i in idxs {
             if !self.cw.filter_incident_by_dim(cell_idx)?.1.contains(&i) {
@@ -151,11 +155,11 @@ impl<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> CellularSheaf<F, T, 
                 return Err(Error::NoRestrictionDefined);
             }
             let restrict = restriction.unwrap();
-            if domain_bases.is_some() && self.data[i].bases.is_some() {
+            if domain_bases.is_some() && self.section_spaces[i].bases.is_some() {
                 results = restrict
                     .adjoint(
                         &domain_bases.as_ref().unwrap(),
-                        &self.data[i].bases.as_ref().unwrap(),
+                        &self.section_spaces[i].bases.as_ref().unwrap(),
                     )?
                     .transform(&cochain[i].0)?;
                 continue;
@@ -163,6 +167,83 @@ impl<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> CellularSheaf<F, T, 
             results = restrict.transpose().transform(&cochain[i].0)?;
         }
         Ok(results)
+    }
+
+    pub fn k_minus_1_coboundary_adjoint(
+        &self,
+        cell_idx: usize,
+        data_idx: usize,
+    ) -> Result<Vec<(Vec<F>, usize)>, Error> {
+        if cell_idx >= self.cw.cells.len() {
+            return Err(Error::InvalidCellIdx);
+        }
+        let domain_bases = &self.section_spaces[cell_idx].bases;
+        let data = &self.section_spaces[cell_idx].data[data_idx];
+        let mut results = Vec::new();
+        for i in self.cw.filter_incident_by_dim(cell_idx)?.0 {
+            let restriction = self.restrictions.get(&(i, cell_idx));
+            if restriction.is_none() {
+                return Err(Error::NoRestrictionDefined);
+            }
+            let restrict = restriction.unwrap();
+            if domain_bases.is_some() && self.section_spaces[i].bases.is_some() {
+                results.push((
+                    restrict
+                        .adjoint(
+                            &domain_bases.as_ref().unwrap(),
+                            &self.section_spaces[i].bases.as_ref().unwrap(),
+                        )?
+                        .transform(data)?,
+                    i,
+                ));
+                continue;
+            }
+            results.push((restrict.transpose().transform(data)?, i));
+        }
+        Ok(results)
+    }
+
+    pub fn k_minus_1_coboundary(
+        &self,
+        cell_idx: usize,
+        cochain: Vec<(Vec<F>, usize)>,
+    ) -> Result<Vec<F>, Error> {
+        if cell_idx >= self.cw.cells.len() {
+            return Err(Error::InvalidCellIdx);
+        }
+        let idxs = cochain.iter().map(|(_, x)| *x).collect::<Vec<_>>();
+        let mut results = vec![F::zero()];
+        for i in idxs {
+            if !self.cw.filter_incident_by_dim(cell_idx)?.0.contains(&i) {
+                return Err(Error::BadCochain);
+            };
+            let restriction = self.restrictions.get(&(i, cell_idx));
+            if restriction.is_none() {
+                return Err(Error::NoRestrictionDefined);
+            }
+            let restrict = restriction.unwrap();
+            results = add_vectors(&results, &restrict.transform(&cochain[i].0)?)?;
+        }
+        Ok(results)
+    }
+
+    pub fn k_up_laplacian(&self, cell_idx: usize, data_idx: usize) -> Result<Vec<F>, Error> {
+        let cochain = self.k_coboundary(cell_idx, data_idx)?;
+        let reverse = self.k_coboundary_adjoint(cell_idx, cochain)?;
+        Ok(reverse)
+    }
+
+    pub fn k_down_laplacian(&self, cell_idx: usize, data_idx: usize) -> Result<Vec<F>, Error> {
+        let cochain = self.k_minus_1_coboundary_adjoint(cell_idx, data_idx)?;
+        let reverse = self.k_minus_1_coboundary(cell_idx, cochain)?;
+        Ok(reverse)
+    }
+
+    pub fn k_laplacian(&self, cell_idx: usize, data_idx: usize) -> Result<Vec<F>, Error> {
+        Ok(add_vectors(
+            &self.k_up_laplacian(cell_idx, data_idx)?,
+            &self.k_down_laplacian(cell_idx, data_idx)?,
+        )?)
     }
 
     pub fn check_glue(
@@ -184,9 +265,9 @@ impl<F: Field, T: Eq + std::hash::Hash + Clone, O: OpenSet> CellularSheaf<F, T, 
         }
         let restrict = restriction.unwrap();
         if restrict
-            .transform(&self.data[start_cell].data[data_idx])
+            .transform(&self.section_spaces[start_cell].data[data_idx])
             .unwrap()
-            != self.data[final_cell].data[data_idx]
+            != self.section_spaces[final_cell].data[data_idx]
         {
             return Ok(false);
         }
